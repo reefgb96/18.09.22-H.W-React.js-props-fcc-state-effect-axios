aeff750e71766e1de800838703648ba7717ecd38
reefgb96 <reef.goldberg904@gmail.com>
2022-09-19 17:21:44 +0300

    fix-next-prev-btn🚀

--- src/pages/RickMortyPage/RickMortyPage.jsx
+++ src/pages/RickMortyPage/RickMortyPage.jsx
@@ -4,15 +4,17 @@ import { RickMortyCards } from "./RickMortyCards";
 
 const RickMortyPage = () => {
   let charactersArr = [];
-  let [dataArr, setDataArr] = useState(charactersArr);
-  let apiLink = "https://rickandmortyapi.com/api/character/";
+  let [dataArr, setDataArr] = useState(null);
+  const [apiLink, setApiLink] = useState(
+    "https://rickandmortyapi.com/api/character/"
+  );
 
   const sendReq = () => {
     axios
       .get(apiLink)
       .then((res) => {
         charactersArr = res.data;
-        setDataArr(charactersArr.results);
+        setDataArr(charactersArr);
       })
       .catch((err) => console.log("err ", err));
   };
@@ -21,34 +23,57 @@ const RickMortyPage = () => {
     sendReq();
   }, []);
 
-  const handleNextPage = () => {
-    setDataArr(dataArr.info.next);
+  const dispalyBtns = () => {
+    if (dataArr != []) {
+      return (
+        <div>
+          <button onClick={handleNextPage}>next</button>
+          <button onClick={handlePrevPage}>prev</button>
+        </div>
+      );
+    }
   };
 
-  const handlePrevPage = () => {};
+  const handleNextPage = () => {
+    setApiLink(dataArr.info.next);
+  };
+  const handlePrevPage = () => {
+    setApiLink(dataArr.info.prev);
+  };
+
+  useEffect(() => {
+    sendReq();
+  }, [apiLink]);
 
   const handleDeleteCard = (id) => {
     let newDataArr = dataArr.filter((item) => item.id !== id);
     setDataArr(newDataArr);
   };
 
+  const displayCards = () => {
+    if (dataArr) {
+      let cardsToDisplay = dataArr.results.map((item) => {
+        return (
+          <RickMortyCards
+            item={item}
+            key={item.id}
+            onDelete={handleDeleteCard}
+          />
+        );
+      });
+      return cardsToDisplay;
+    }
+  };
+
+  useEffect(() => {
+    displayCards();
+  }, [dataArr]);
+
   return (
     <>
-      {/* <div>
-        <button onClick={handleNextPage}>next</button>
-        <button onClick={handlePrevPage}>prev</button>
-      </div> */}
-      <div className="row row-cols-1 row-cols-md-2 g-4">
-        {dataArr.map((item) => {
-          return (
-            <RickMortyCards
-              item={item}
-              key={item.id}
-              onDelete={handleDeleteCard}
-            />
-          );
-        })}
-      </div>
+      {dispalyBtns()}
+
+      <div className="row row-cols-1 row-cols-md-2 g-4">{displayCards()}</div>
     </>
   );
 };
